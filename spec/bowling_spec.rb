e31d09d58a66778e00902f71d35f83c6c96423f6
Eric Lawler <eric@lawler.io>
2019-10-01 14:34:51 -0700

    Add more tests for gutterballs and two different "all spares" conditions

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -5,11 +5,18 @@
 require 'bowling'
 
 describe Bowling, "#score" do
+  context 'with only gutterballs' do
+    it 'sums the pin count for each throw' do
+      bowling = Bowling.new
+      20.times { bowling.hit(0) }
+      expect(bowling.score).to eq 0
+    end
+  end
   context 'with no strikes or spares' do
     it 'sums the pin count for each throw' do
       bowling = Bowling.new
-      10.times { bowling.hit(4) }
-      expect(bowling.score).to eq 40
+      20.times { bowling.hit(4) }
+      expect(bowling.score).to eq 80
     end
   end
   context 'with only strikes' do
@@ -17,6 +24,30 @@ describe Bowling, "#score" do
       bowling = Bowling.new
       12.times { bowling.hit(10) }
       expect(bowling.score).to eq 300
+    end
+  end
+  context 'with only spares' do
+    it 'throws strike-spares' do
+      bowling = Bowling.new
+      10.times do
+        bowling.hit(0)
+        bowling.hit(10)
+      end
+
+      # final throw in frame 10
+      bowling.hit(0)
+      expect(bowling.score).to eq 100
+    end
+    it 'throws 5-pin spares' do
+      bowling = Bowling.new
+      10.times do
+        bowling.hit(5)
+        bowling.hit(5)
+      end
+
+      # final throw in frame 10
+      bowling.hit(5)
+      expect(bowling.score).to eq 150
     end
   end
 end
